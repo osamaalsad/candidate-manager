dcc7f39f02599aeaa50a5732e8684db7213a87ce
osamaalsad <o.alsad1998@gmail.com>
2023-11-08 20:44:21 +0300

    add authentication on generate report endpoint

--- app/routers/generate_report.py
+++ app/routers/generate_report.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from app.security.jwt import get_current_user
 from app.services.reports import ReportService
 router = APIRouter()
 
 
-@router.get("/generate-report")
+@router.get("/generate-report", dependencies=[Depends(get_current_user)])
 async def generate_report():
     return await ReportService.generate_report()
