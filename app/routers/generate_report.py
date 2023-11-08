436b49a481a1a70afb14a88f17869797bdc5d432
osamaalsad <o.alsad1998@gmail.com>
2023-11-08 20:03:01 +0300

    add report service and use it in routes

--- app/routers/generate_report.py
+++ app/routers/generate_report.py
@@ -1,29 +1,8 @@
-import csv
-from fastapi import APIRouter, HTTPException
-from fastapi.responses import FileResponse
-from app.db import candidates_collection  # Import your candidates collection
+from fastapi import APIRouter
+from app.services.reports import ReportService
 router = APIRouter()
 
 
 @router.get("/generate-report")
 async def generate_report():
-    # Retrieve candidate data from the database
-    candidates = await candidates_collection.find({}).to_list(None)
-
-    if not candidates:
-        raise HTTPException(status_code=404, detail="No candidates found in the database.")
-
-    # Define the CSV file path
-    csv_file_path = "candidates_report.csv"
-
-    # Create and write data to the CSV file
-    with open(csv_file_path, "w", newline="") as csv_file:
-        fieldnames = ["_id", "first_name", "last_name", "email", "UUID", "career_level", "job_major", "years_of_experience", "degree_type", "skills", "nationality", "city", "salary", "gender"]
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-        writer.writeheader()
-        for candidate in candidates:
-            writer.writerow(candidate)
-
-    # Serve the CSV file as a response
-    return FileResponse(csv_file_path, headers={"Content-Disposition": "attachment; filename=candidates_report.csv"})
+    return ReportService.generate_report()
