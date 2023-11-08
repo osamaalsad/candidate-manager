de7f004c6f1723300e253ba15b49527173c141a6
osamaalsad <o.alsad1998@gmail.com>
2023-11-08 20:00:20 +0300

    modify user model and add password

--- app/models/users.py
+++ app/models/users.py
@@ -1,12 +1,18 @@
 # app/models.py
 from pydantic import BaseModel
+from passlib.hash import pbkdf2_sha256
 
 
 class UserCreate(BaseModel):
     first_name: str
     last_name: str
     email: str
+    password: str
+
+    def hash_password(self):
+        self.password = pbkdf2_sha256.hash(self.password)
 
 
 class UserLogin(BaseModel):
     email: str
+    password: str
