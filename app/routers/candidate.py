adfb3addde1a8284acd91d77b7dd193e2cc6465b
osamaalsad <o.alsad1998@gmail.com>
2023-11-08 20:02:31 +0300

    divide the candidate file to route and service

--- app/routers/candidate.py
+++ app/routers/candidate.py
@@ -1,59 +1,37 @@
-# app/routers/candidate.py
-from bson import ObjectId
-from fastapi import APIRouter, HTTPException
-from fastapi import Depends
+from fastapi import APIRouter, Depends
 
-from app.db import candidates_collection
 from app.models.candidates import CandidateCreate
 from app.security.jwt import get_current_user
+from app.services.candidates import CandidateService
 
 router = APIRouter()
+candidate_service = CandidateService()
 
 
-@router.post("/candidates", response_model=CandidateCreate)
-async def create_candidate(candidate: CandidateCreate, current_user: dict = Depends(get_current_user)):
-    # Insert the candidate data into MongoDB
-    result = await candidates_collection.insert_one(candidate.model_dump())
-
-    # Get the inserted candidate's ID
-    inserted_id = result.inserted_id
-
-    return {**candidate.model_dump(), "_id": str(inserted_id)}
+@router.post("/candidates", response_model=CandidateCreate, dependencies=[Depends(get_current_user)])
+async def create_candidate(candidate: CandidateCreate):
+    return candidate_service.create_candidate(candidate)
 
 
-@router.get("/candidates/{candidate_id}", response_model=CandidateCreate)
-async def get_candidate(candidate_id: str, current_user: dict = Depends(get_current_user)):
-    print(f'ObjectId("{candidate_id}")')
-    candidate = await candidates_collection.find_one({"_id": ObjectId(candidate_id)})
-
-    if candidate:
-        return {**candidate, "_id": str(candidate.pop("_id"))}
-    else:
-        raise HTTPException(status_code=404, detail="Candidate not found")
+@router.get("/candidates/{candidate_id}", response_model=CandidateCreate, dependencies=[Depends(get_current_user)])
+async def get_candidate(candidate_id: str):
+    return candidate_service.get_candidate(candidate_id)
 
 
-@router.put("/candidates/{candidate_id}", response_model=CandidateCreate)
+@router.put("/candidates/{candidate_id}", response_model=CandidateCreate, dependencies=[Depends(get_current_user)])
 async def update_candidate(
         candidate_id: str,
         updated_candidate: CandidateCreate,
         current_user: dict = Depends(get_current_user)
 ):
-    # Update the candidate data in MongoDB
-    result = await candidates_collection.update_one(
-        {"_id": ObjectId(candidate_id)}, {"$set": updated_candidate.model_dump()}
-    )
-
-    if result.modified_count:
-        return updated_candidate
-    else:
-        raise HTTPException(status_code=404, detail="Candidate not found")
+    return candidate_service.update_candidate(candidate_id, updated_candidate)
 
 
-@router.delete("/candidates/{candidate_id}")
-async def delete_candidate(candidate_id: str, current_user: dict = Depends(get_current_user)):
-    result = await candidates_collection.delete_one({"_id": ObjectId(candidate_id)})
+@router.delete("/candidates/{candidate_id}", dependencies=[Depends(get_current_user)])
+async def delete_candidate(candidate_id: str):
+    return candidate_service.delete_candidate(candidate_id)
 
-    if result.deleted_count:
-        return {"message": "Candidate deleted"}
-    else:
-        raise HTTPException(status_code=404, detail="Candidate not found")
+
+@router.get("/all-candidates", response_model=list, dependencies=[Depends(get_current_user)])
+async def get_all_candidates(search_keyword: str = None):
+    return candidate_service.get_all_candidate(search_keyword)
---
+++ app/services/candidates.py
@@ -0,0 +1,68 @@
+from bson import ObjectId
+from fastapi import HTTPException
+
+from app.db import candidates_collection
+from app.models.candidates import CandidateCreate
+
+
+class CandidateService:
+
+    def create_candidate(self, candidate: CandidateCreate):
+        # Insert the candidate data into MongoDB
+        result = candidates_collection.insert_one(candidate.model_dump())
+
+        # Get the inserted candidate's ID
+        inserted_id = result.inserted_id
+
+        return CandidateCreate(**candidate.model_dump(), _id=str(inserted_id))
+
+    def get_candidate(self, candidate_id: str):
+        candidate = candidates_collection.find_one({"_id": ObjectId(candidate_id)})
+
+        if candidate:
+            return CandidateCreate(**{**candidate, "_id": str(candidate.pop("_id"))})
+        else:
+            raise HTTPException(status_code=404, detail="Candidate not found")
+
+    def update_candidate(self, candidate_id: str, updated_candidate: CandidateCreate):
+        # Update the candidate data in MongoDB
+        result = candidates_collection.update_one(
+            {"_id": ObjectId(candidate_id)}, {"$set": updated_candidate.model_dump()}
+        )
+
+        if result.modified_count:
+            return updated_candidate
+        else:
+            raise HTTPException(status_code=404, detail="Candidate not found")
+
+    def delete_candidate(self, candidate_id: str):
+        result = candidates_collection.delete_one({"_id": ObjectId(candidate_id)})
+
+        if result.deleted_count:
+            return {"message": "Candidate deleted"}
+        else:
+            raise HTTPException(status_code=404, detail="Candidate not found")
+
+    async def get_all_candidate(self, search_keyword: str = None):
+        query = {}  # Empty query to retrieve all candidates
+
+        if search_keyword:
+            # Create a query to search for the keyword in all candidate fields
+            query = {
+                "$or": [
+                    {"first_name": {"$regex": search_keyword, "$options": "i"}},
+                    {"last_name": {"$regex": search_keyword, "$options": "i"}},
+                    {"email": {"$regex": search_keyword, "$options": "i"}},
+                    {"career_level": {"$regex": search_keyword, "$options": "i"}},
+                    {"job_major": {"$regex": search_keyword, "$options": "i"}},
+                    {"nationality": {"$regex": search_keyword, "$options": "i"}},
+                    {"city": {"$regex": search_keyword, "$options": "i"}},
+                ]
+            }
+
+        candidates = []
+
+        async for candidate in candidates_collection.find(query):
+            candidates.append({**candidate, "_id": str(candidate.pop("_id"))})
+
+        return candidates
