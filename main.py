10a130b687fc681722993a4704b94c06265e4782
osamaalsad <o.alsad1998@gmail.com>
2023-11-08 20:04:12 +0300

    delete all candidate route

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import health, user, candidate, all_candidates, generate_report
+from app.routers import health, user, candidate, generate_report
 from app.security import auth
 
 app = FastAPI()
@@ -8,5 +8,4 @@ app.include_router(health.router)
 app.include_router(auth.router, prefix="/auth")
 app.include_router(user.router, prefix="/user", tags=["user"])
 app.include_router(candidate.router, prefix="/candidate", tags=["candidate"])
-app.include_router(all_candidates.router, prefix="/all-candidates", tags=["all-candidates"])
 app.include_router(generate_report.router, prefix="/generate-report", tags=["generate-report"])
